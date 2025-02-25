593a4a19fa40be071267d139e86d37ae2754ae39
Felipe nast loyola <felipenastloyola@Felipes-MacBook-Air.local>
2025-02-25 17:26:12 +0100

    add sorting

--- products/views.py
+++ products/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.db.models import Q
+from django.urls import reverse
 from .models import Product
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, "products/product_list.html", {"products": products})
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            try:
+                categories = [int(cat) for cat in categories]  # Convert category values to integers
+                products = products.filter(category__in=categories)
+            except ValueError:
+                messages.error(request, "Invalid category filter!")
+
+        if 'q' in request.GET:  # <-- Ensure correct indentation here
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+    }
+
+    return render(request, "products/product_list.html", context)
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
