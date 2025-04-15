ef19135c5aa6e36977408aa3ef6770acbf123646
fenasti <felipenast@gmail.com>
2025-04-15 21:25:45 +0200

    Update max_length for product_size field

--- checkout/models.py
+++ checkout/models.py
@@ -172,7 +172,7 @@ class OrderLineItem(models.Model):
         on_delete=models.CASCADE
     )
     product_size = models.CharField(
-        max_length=2,
+        max_length=5,
         null=True,
         blank=True
     )  # Sizes: XS, S, M, L, XL
