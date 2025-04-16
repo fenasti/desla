10a7d80113c0b2a0bd635be995ee8d2ae0c7e79c
fenasti <felipenast@gmail.com>
2025-04-16 15:26:17 +0200

    Add missing migrations for checkout app

--- checkout/migrations/0003_alter_order_order_number.py
+++ checkout/migrations/0003_alter_order_order_number.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('checkout', '0001_initial'),
+        ('checkout', '0002_alter_order_order_number'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='order',
             name='order_number',
-            field=models.CharField(default='A17EFD9F84444544AE8F777A', editable=False, max_length=24, unique=True),
+            field=models.CharField(default='59A4F239465640FA8C4C4E05', editable=False, max_length=24, unique=True),
         ),
     ]
